d5006e3bcd144d059b9b13489cf60a13acf35888
Ramu <rainwingsd@gmail.com>
2026-07-11 21:08:05 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        if (strs.empty()) return "";
+       string res;
+       for(string &s : strs)
+       {
+        res.append(to_string(s.size()));
+        res.push_back('#');
+        res.append(s);
+       }
+       return res;
+    }
+
+    vector<string> decode(string s) {
+        if (s.empty()) return {};
+      vector<string> res;
+      int i=0;
+      while(i<s.size())
+      {
+        int j=i;
+        while(s[j]!='#')
+        {
+            j++;
+        }
+        int length=stoi(s.substr(i,j-i));
+        i=j+1;
+        j=i+length;
+        res.push_back(s.substr(i,length));
+        i=j;
+      }
+      return res;
+    }
+};
